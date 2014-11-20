d0926dbae1b2aa6a61120d2993d44775d4001942
Ivan Avraamov <avraamov@tadatuta.com>
2014-11-20 12:18:32 +0300

    templates fix

--- wp-content/themes/eddiemachado-bones-611c04e/home-template.php
+++ wp-content/themes/eddiemachado-bones-611c04e/home-template.php
@@ -36,10 +36,45 @@
                     <?php } ?>
                 
                 <?php } else { ?>
-                    <a class="btn btn_lg btn_warning btn_login" href="/admin">Войдите на сайт</a>
-                    <a class="btn btn_lg btn_success btn_sign" href="/registration">Получить доступ</a>
+                    <!-- <a class="btn btn_lg btn_warning btn_login" href="/admin">Войдите на сайт</a>
+                    <a class="btn btn_lg btn_success btn_sign" href="/registration">Получить доступ</a> -->
                     <div class="home-content" style="text-align: center;">
                         <?php the_content(); ?>
+                        <div class="btn btn_lg btn_trans show_form">Вход для сертифицированных пользователей</div>
+                    </div>
+                    <div class="login__form hidden">
+                        <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>" method="post">
+                            <p>
+                                <label for="user_login"><?php _e('Username') ?><br />
+                                <input type="text" name="log" id="user_login" class="input" value="<?php echo esc_attr($user_login); ?>" size="20" /></label>
+                            </p>
+                            <p>
+                                <label for="user_pass"><?php _e('Password') ?><br />
+                                <input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+                            </p>
+                            <?php
+                            /**
+                             * Fires following the 'Password' field in the login form.
+                             *
+                             * @since 2.1.0
+                             */
+                            do_action( 'login_form' );
+                            ?>
+                            <!-- <p class="note_small">Что бы получить доступ </p> -->
+                            <p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked( $rememberme ); ?> /> <?php esc_attr_e('Remember Me'); ?></label></p>
+                            <p class="submit">
+                                <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Log In'); ?>" />
+                        <?php   if ( $interim_login ) { ?>
+                                <input type="hidden" name="interim-login" value="1" />
+                        <?php   } else { ?>
+                                <input type="hidden" name="redirect_to" value="<?php echo esc_attr($redirect_to); ?>" />
+                        <?php   } ?>
+                        <?php   if ( $customize_login ) : ?>
+                                <input type="hidden" name="customize-login" value="1" />
+                        <?php   endif; ?>
+                                <input type="hidden" name="testcookie" value="1" />
+                            </p>
+                        </form>
                     </div>
                 <?php } ?>
 
--- wp-content/themes/eddiemachado-bones-611c04e/upload-template.php
+++ wp-content/themes/eddiemachado-bones-611c04e/upload-template.php
@@ -76,7 +76,11 @@
                 <li id="draggableD5" class="itemlist_item item_list__mid draggable" style="left: 130px; top: -192px;">D5</li>
                 <li id="draggableD4" class="itemlist_item item_list__mid draggable" style="left: 148px; top: -274px;">D4</li>
             </ul>
-            <ul class="itemlist itemlist-two"><!--
+            <ul class="itemlist itemlist-two cropme" style="width: 392px; height: 600px;">
+                <script>
+                    jQuery('.cropme').simpleCropper();
+                </script>
+            <!--
                 <li class="itemlist_item">1</li>
                 <li class="itemlist_item">2</li>
                 <li class="itemlist_item">3</li>
@@ -85,8 +89,39 @@
             --></ul>
             </div>
         <?php } else { ?>
-            <div class="login-area">
-               <p>Пожалуйста <a href="/registration">Зарегестрируйтесь</a> сайте</br> или <a href="/admin">Авторизуйтесь</a></p> 
+            <div class="login__form">
+                <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>" method="post">
+                    <p>
+                        <label for="user_login"><?php _e('Username') ?><br />
+                        <input type="text" name="log" id="user_login" class="input" value="<?php echo esc_attr($user_login); ?>" size="20" /></label>
+                    </p>
+                    <p>
+                        <label for="user_pass"><?php _e('Password') ?><br />
+                        <input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+                    </p>
+                    <?php
+                    /**
+                     * Fires following the 'Password' field in the login form.
+                     *
+                     * @since 2.1.0
+                     */
+                    do_action( 'login_form' );
+                    ?>
+                    <!-- <p class="note_small">Что бы получить доступ </p> -->
+                    <p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked( $rememberme ); ?> /> <?php esc_attr_e('Remember Me'); ?></label></p>
+                    <p class="submit">
+                        <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Log In'); ?>" />
+                <?php   if ( $interim_login ) { ?>
+                        <input type="hidden" name="interim-login" value="1" />
+                <?php   } else { ?>
+                        <input type="hidden" name="redirect_to" value="<?php echo esc_attr($redirect_to); ?>" />
+                <?php   } ?>
+                <?php   if ( $customize_login ) : ?>
+                        <input type="hidden" name="customize-login" value="1" />
+                <?php   endif; ?>
+                        <input type="hidden" name="testcookie" value="1" />
+                    </p>
+                </form>
             </div>
         <?php } ?>
 
