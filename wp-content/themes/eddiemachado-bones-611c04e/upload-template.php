1c13002eb512dca64966bab54e5f0272dc6c5063
Ivan Avraamov <avraamov@tadatuta.com>
2014-11-28 11:47:29 +0300

    img catch 5

--- wp-content/themes/eddiemachado-bones-611c04e/upload-template.php
+++ wp-content/themes/eddiemachado-bones-611c04e/upload-template.php
@@ -67,7 +67,7 @@
                         <label>Разрешение изображения</label> <input id="filedim" name="mci_filedim" type="text" />
                         <label>Ширина</label> <input id="w" name="mci_w" type="text" />
                         <label>Высота</label> <input id="h" name="mci_h" type="text" /></div>
-                        <input type="submit" value="Загрузить" name="mci_magic" />
+                        <input type="submit" value="Редактировать фото" name="mci_magic" />
                         </div>
                         </form>
                     
