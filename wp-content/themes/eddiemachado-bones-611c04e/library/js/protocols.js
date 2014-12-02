50bb1e3de5ffa4419241521dbe382768553294c5
Ivan Avraamov <avraamov@tadatuta.com>
2014-12-02 17:30:02 +0300

    phase 2 fix

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -7,7 +7,7 @@
     //фаза 1
         count_animation = 1;
         curPhase = setInterval(function(){
-            if (count_animation <= 120){
+            if (count_animation <= 30){
                 cur_animation_val += 6;
                 jQuery('#draggableClean').css('transform', 'rotate(-'+cur_animation_val+'deg)');
                 jQuery('#draggable2, #draggableS2, #draggableS2_1').css('background', 'red');
@@ -19,7 +19,8 @@
             }
         }, 1000);
     //фаза 2
-        cur_animation_val = 0
+        cur_animation_val = 0;
+        count_animation = 1;
         jQuery('#draggableClean_2').css('left', jQuery('#draggableS2').css('left')+50+'px');
         jQuery('#draggableClean_3').css('left', jQuery('#draggableS2_1').css('left')+50+'px');
         jQuery('#draggableClean_2, #draggableClean_3').removeClass('inopaciti');
