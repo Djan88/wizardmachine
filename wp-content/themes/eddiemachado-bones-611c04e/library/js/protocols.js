c0080a866d1b128562f1b7d71749f08fd3095b7b
Ivan Avraamov <avraamov@tadatuta.com>
2014-12-02 17:37:01 +0300

    phase 2 fix-2

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -1,41 +1,39 @@
     var count_animation = 1,
         cur_animation_val = 0,
-        curPhase,
+        phaseOne,
+        phaseTwo,
         v2;
 
     v2 = function(){
     //фаза 1
         count_animation = 1;
-        curPhase = setInterval(function(){
+        phaseOne = setInterval(function(){
             if (count_animation <= 30){
                 cur_animation_val += 6;
                 jQuery('#draggableClean').css('transform', 'rotate(-'+cur_animation_val+'deg)');
                 jQuery('#draggable2, #draggableS2, #draggableS2_1').css('background', 'red');
                 count_animation += 1;
             } else {
-                clearInterval(curPhase);
+                clearInterval(phaseOne);
                 count_animation = 1;
                 jQuery('#draggable2, #draggableS2, #draggableS2_1').css('background', 'transparent');
-            }
-        }, 1000);
     //фаза 2
-        cur_animation_val = 0;
-        count_animation = 1;
-        jQuery('#draggableClean_2').css('left', jQuery('#draggableS2').css('left')+50+'px');
-        jQuery('#draggableClean_3').css('left', jQuery('#draggableS2_1').css('left')+50+'px');
-        jQuery('#draggableClean_2, #draggableClean_3').removeClass('inopaciti');
-        curPhase = setInterval(function(){
-            if (count_animation <= 60){
-                cur_animation_val += 6;
-                jQuery('#draggableClean').css('transform', 'rotate(-'+cur_animation_val+'deg)');
-                jQuery('#draggableClean_2, #draggableClean_2').css('background', 'red');
-                count_animation += 1;
-            } else {
-                clearInterval(curPhase);
-                count_animation = 1;
-                jQuery('#draggableClean_2, #draggableClean_3').addClass('inopaciti');
-                jQuery('#draggableClean_2, #draggableClean_2').css('background', 'transparent');
+                cur_animation_val = 0;
+                jQuery('#draggableClean_2').css('left', jQuery('#draggableS2').css('left')+50+'px');
+                jQuery('#draggableClean_3').css('left', jQuery('#draggableS2_1').css('left')+50+'px');
+                jQuery('#draggableClean_2, #draggableClean_3').removeClass('inopaciti');
+                phaseTwo = setInterval(function(){
+                    if (count_animation <= 60){
+                        cur_animation_val += 6;
+                        jQuery('#draggableClean_2, #draggableClean_2').css('transform', 'rotate(-'+cur_animation_val+'deg)');
+                        count_animation += 1;
+                    } else {
+                        clearInterval(phaseTwo);
+                        count_animation = 1;
+                        jQuery('#draggableClean_2, #draggableClean_3').addClass('inopaciti');
+                        jQuery('#draggableClean_2, #draggableClean_2').css('background', 'transparent');
+                    }
+                }, 1000);
             }
         }, 1000);
-
     }
