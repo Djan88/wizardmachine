2fff0ee85ebb4d341e01181c156b0eed52442f1b
Ivan Avraamov <avraamov@tadatuta.com>
2014-12-03 16:06:00 +0300

    phase 4

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -74,6 +74,39 @@
                                     transform: 'rotate(0deg) scale(1)'
                                 });
                                 jQuery('#draggableS4, #draggable4').css('background', 'transparent');
+    //фаза 4
+                                cur_animation_val = 0;
+                                jQuery('#draggableClean_2').css({
+                                    left: parseFloat(jQuery('#draggableS5').css('left'))+70+'px',
+                                    top: parseFloat(jQuery('#draggableS5').css('top'))-69+'px'
+                                });
+                                jQuery('#draggableClean_3').css({
+                                    left: parseFloat(jQuery('#draggableS6').css('left'))+70+'px',
+                                    top: parseFloat(jQuery('#draggableS6').css('top'))-69+'px'
+                                });
+                                jQuery('#draggableClean_2, #draggableClean_3').css('background', 'url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/lovushka.jpg) 0 0/100% no-repeat');
+                                jQuery('#draggableClean_2, #draggableClean_3').removeClass('inopaciti');
+                                phaseThree = setInterval(function(){
+                                    if (count_animation <= 10){ //120
+                                        cur_animation_val += 6;
+                                        jQuery('#draggableClean_2, #draggableClean_3').css('transform', 'rotate(-'+cur_animation_val+'deg) scale(2)');
+                                        jQuery('#draggable5, #draggableD5').css('background', 'red');
+                                        jQuery('#draggable5, #draggableD5').css('background', 'transparent');
+                                        if (count_animation >= 5 && count_animation <= 10){ //60
+                                            jQuery('#draggableS5, #draggableS6').css('background', 'red');
+                                        }
+                                        count_animation += 1;
+                                    } else {
+                                        clearInterval(phaseThree);
+                                        count_animation = 1;
+                                        jQuery('#draggableClean_2, #draggableClean_3').addClass('inopaciti');
+                                        jQuery('#draggableClean_2, #draggableClean_3').css({
+                                            background: 'transparent',
+                                            transform: 'rotate(0deg) scale(1)'
+                                        });
+                                        jQuery('#draggable5, #draggableD5, #draggableS5, #draggableS6').css('background', 'transparent');
+                                    }
+                                }, 1000);
                             }
                         }, 1000);
                     }
