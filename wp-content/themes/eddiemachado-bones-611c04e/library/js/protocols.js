6146fcdb2f6e7a0df7307aff6c85f03d3f96a8ee
Ivan Avraamov <avraamov@tadatuta.com>
2014-12-04 11:13:43 +0300

    phormuls adding fix 4

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -9,7 +9,7 @@
     //фаза 1
         count_animation = 1;
         phaseOne = setInterval(function(){
-            if (count_animation <= 10){                                                                         //120
+            if (count_animation <= 120){                                                                         //120
                 cur_animation_val += 6;
                 jQuery('#draggableClean').css({
                     transform: 'rotate(-'+cur_animation_val+'deg) scale(2)',
@@ -44,7 +44,7 @@
                 jQuery('#draggableS1').css('color', 'transparent');
                 jQuery('#draggableClean_2, #draggableClean_3').removeClass('inopaciti');
                 phaseTwo = setInterval(function(){
-                    if (count_animation <= 10){                                                                 //120
+                    if (count_animation <= 120){                                                                 //120
                         cur_animation_val += 6;
                         jQuery('#draggableClean_2, #draggableClean_3, #draggableS1').css('transform', 'rotate(-'+cur_animation_val+'deg) scale(2)');
                         count_animation += 1;
@@ -66,7 +66,7 @@
                         });
                         jQuery('#draggableClean_2').removeClass('inopaciti');
                         phaseThree = setInterval(function(){
-                            if (count_animation <= 10){                                                         //120
+                            if (count_animation <= 120){                                                         //120
                                 cur_animation_val += 6;
                                 jQuery('#draggableClean_2').css('transform', 'rotate(-'+cur_animation_val+'deg) scale(2)');
                                 jQuery('#draggableS4').css('background', 'url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/povregdenie_demona.jpg) 0 0/100% no-repeat');
@@ -75,7 +75,7 @@
                                     color: 'transparent',
                                     transform: 'scale(2)'
                                 });
-                                if (count_animation >= 5 && count_animation <= 10){                               //60
+                                if (count_animation >= 60 && count_animation <= 120){                               //60
                                     jQuery('#draggableS4').css('background', 'url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/filtr.jpg) 0 0/100% no-repeat');
                                 }
                                 count_animation += 1;
@@ -106,13 +106,13 @@
                                     color: 'transparent'
                                 });
                                 phaseThree = setInterval(function(){
-                                    if (count_animation <= 10){                                                //120
+                                    if (count_animation <= 120){                                                //120
                                         cur_animation_val += 6;
                                         jQuery('#draggableClean_2, #draggableClean_3').css('transform', 'rotate(-'+cur_animation_val+'deg) scale(2)');
                                         jQuery('#draggable5, #draggableD5').css('background', 'url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/disfunction.jpg) 0 0/100% no-repeat');
                                         jQuery('#draggableS5, #draggableS6').css('background', 'url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/povregdenie_demona.jpg) 0 0/100% no-repeat');
                                         jQuery('#draggable5, #draggableD5, #draggableS5, #draggableS6').css('color', 'transparent');
-                                        if (count_animation >= 5 && count_animation <= 10){                      //60
+                                        if (count_animation >= 60 && count_animation <= 120){                      //60
                                             jQuery('#draggableS5, #draggableS6').css('background', 'url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/filtr.jpg) 0 0/100% no-repeat');
                                         }
                                         count_animation += 1;
