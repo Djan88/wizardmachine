06b5a7bc383dec3941168fc63e2828f89c7902b2
Ivan Avraamov <avraamov@tadatuta.com>
2014-12-07 17:26:11 +0300

    triangle fix-4

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -133,7 +133,7 @@
     //Фаза 5
                                         cur_animation_val = 0;
                                         count_animation = 1;
-                                        jQuery('#triangle_1').removeClass('hidden');
+                                        jQuery('#draggable_1').find('svg').removeClass('hidden');
                                         var a = new Vivus('triangle_1', {type: 'delayed', duration: 100});
                                         a.play();
                                     }
