58850d9c2120727e6c70b52cf0bb796e7217ebf1
Ivan Avraamov <avraamov@tadatuta.com>
2014-12-10 12:18:10 +0300

    v3 ph7 fix4

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -735,7 +735,7 @@
                                                                                                                                                         cur_animation_val = 30;
                                                                                                                                                         count_animation = 1;
                                                                                                                                                         phaseSeven_one = setInterval(function(){
-                                                                                                                                                            if (count_animation <= 14){                                                                         //14
+                                                                                                                                                            if (count_animation <= 15){                                                                         //15
                                                                                                                                                                 cur_animation_val += 3;
                                                                                                                                                                 jQuery('#draggable1').css({
                                                                                                                                                                     transform: 'rotate(-'+cur_animation_val+'deg) scale(2)',
