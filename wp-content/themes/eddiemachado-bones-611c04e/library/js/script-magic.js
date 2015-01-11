6e6db0335a209cab768d108427ec7e878e760f89
Ivan Avraamov <avraamov@tadatuta.com>
2015-01-11 10:39:16 +0300

    alerts-fix-6

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
@@ -113,7 +113,7 @@ jQuery( ".btn__wizard" ).on('click', function(event) {
     pointsStatus = true;
     checkPoints();
     if(pointsStatus == false){
-        swal("Не все зоны перенесены", "Перед началом процедуры необходимо перенести все зоны", "warning")
+        swal("Не все зоны перенесены", "Перед началом процедуры необходимо перенести все зоны", "success")
     } else {
         jQuery(this)
             .addClass('btn__wizard_inAction')
