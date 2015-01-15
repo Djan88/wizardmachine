787e4473dba925ec4780bd20f616b672256fb0e5
Ivan Avraamov <avraamov@tadatuta.com>
2015-01-15 15:03:21 +0300

    fast-protocol

--- wp-content/themes/eddiemachado-bones-611c04e/upload-template.php
+++ wp-content/themes/eddiemachado-bones-611c04e/upload-template.php
@@ -25,6 +25,12 @@
 
 
             <?php if(current_user_can('contributor') || current_user_can('administrator')) { ?>
+                <div class="fast-protocol-wrap clearfix">
+                  <div class="btn btn_sm btn_warning" data-fast="V2">V2</div> 
+                  <div class="btn btn_sm btn_warning" data-fast="V3">V3</div> 
+                  <div class="btn btn_sm btn_warning" data-fast="V4">V4</div> 
+                  <div class="btn btn_sm btn_warning" data-fast="V5">V5</div>
+                </div>
 
                 <div class="steps clearfix">
                     <!-- <div class="steps-center"><hr></div>
