db5ad17ee931b53f7f571b966a026a3c13c67aca
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-02-09 12:51:47 +0300

    main btns-1

--- wp-content/themes/eddiemachado-bones-611c04e/home-template.php
+++ wp-content/themes/eddiemachado-bones-611c04e/home-template.php
@@ -46,9 +46,9 @@
                     <div class="home-content" style="text-align: center;">
                         <?php the_content(); ?>
                         <div class="btn btn_lg btn_trans show_form">Вход для сертифицированных пользователей</div><br>
-                        <a href="/" target="_blank" class="button button-primary button-large">Видео"</a>
-                        <a href="/" target="_blank" class="button button-primary button-large">Книга "Визард терапия"</a>
-                        <a href="/" target="_blank" class="button button-primary button-large">Книга "Биологическое центрирование"</a>
+                        <a href="/" target="_blank" class="button button-primary button-large btn__main">Видео"</a>
+                        <a href="/" target="_blank" class="button button-primary button-large btn__main">Книга "Визард терапия"</a>
+                        <a href="/" target="_blank" class="button button-primary button-large btn__main">Книга "Биологическое центрирование"</a>
                     </div>
                     <div class="login__form hidden">
                         <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>" method="post">
