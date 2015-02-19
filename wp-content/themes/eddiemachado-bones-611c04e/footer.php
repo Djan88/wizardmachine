a4f3af732550f320ddadd0e7c04ed04e1ea843c5
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-02-19 13:22:56 +0300

    resurs-12

--- wp-content/themes/eddiemachado-bones-611c04e/footer.php
+++ wp-content/themes/eddiemachado-bones-611c04e/footer.php
@@ -36,6 +36,7 @@
         <script src="<?php bloginfo('template_url'); ?>/library/js/exif.js"></script>
         <script src="<?php bloginfo('template_url'); ?>/library/js/vivus.min.js"></script>
         <script src='<?php bloginfo('template_url'); ?>/library/js/sweet-alert.min.js'></script>
+        <script src='<?php bloginfo('template_url'); ?>/library/js/buzz.min.js'></script>
         <script src='<?php bloginfo('template_url'); ?>/library/js/protocols.js'></script>
         <script src='<?php bloginfo('template_url'); ?>/library/js/script-magic.js'></script>
         <?php if(is_front_page()) { ?>
