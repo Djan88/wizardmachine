c76968e6eeb8a5e9554a4f80568682b8c0021409
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-02-19 15:30:57 +0300

    resurs-37

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -3000,11 +3000,11 @@ resource = function(){
         jQuery('#itemlist-two').on('click', function(e) {
             var v7x = e.offsetX==undefined?e.layerX:e.offsetX;
             var v7y = e.offsetY==undefined?e.layerY:e.offsetY;
-            console.log(v7x +'x'+ v7y);
-            v7x= v7x-20;
+            console.log(resourceActive);
+            v7x= v7x-18;
             jQuery('#itemlist-two').append('<li id="draggable3'+resorceGlobalCount+'" class="itemlist_item item_list__mid draggable ui-draggable ui-draggable-handle" style="left: '+v7x+'px; top: '+v7y+'px; color: transparent; border-color: transparent; opacity: 0.8; transform: scale(1); border-width: 1px; padding-top: 4px; z-index: 1000; background: url(http://yuchikurov.ru/wp-content/themes/eddiemachado-bones-611c04e/library/images/resurs.png) 0px 0px / 100% no-repeat rgb(255, 255, 255);">V3</li>');
             setTimeout(function(){
-                resorceGlobalCount
+                resorceGlobalCount++;
                 jQuery('#draggable3'+resorceGlobalCount).remove();
             }, 4000);
         });
@@ -3013,7 +3013,7 @@ resource = function(){
         if (resorceGlobal >= 1){
         jQuery('body').on('click', function(event) {
             resorceGlobal = 30;
-            console.log('test');
+            // console.log('test');
             // console.log(event.pageX);
             // console.log(event.pageY);
         });
