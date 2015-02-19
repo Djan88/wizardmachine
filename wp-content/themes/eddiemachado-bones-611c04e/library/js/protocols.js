4b2968428543ad6fe8391046263756a1745bb60c
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-02-19 15:46:11 +0300

    resurs-43

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -2995,7 +2995,6 @@ v5 = function(){
 
 resource = function(){
     resorceGlobal = 30;
-    resourceActive = true;
     phaseOne = setInterval(function(){
         if (resorceGlobal >= 1){
             jQuery('#itemlist-two').on('click', function(e) {
@@ -3006,7 +3005,7 @@ resource = function(){
                 jQuery('#itemlist-two').append('<li id="draggable3'+resorceGlobalCount+'" class="itemlist_item item_list__mid draggable ui-draggable ui-draggable-handle" style="left: '+v7x+'px; top: '+v7y+'px; color: transparent; border-color: transparent; opacity: 0.8; transform: scale(1); border-width: 1px; padding-top: 4px; z-index: 1000; background: url(http://yuchikurov.ru/wp-content/themes/eddiemachado-bones-611c04e/library/images/resurs.png) 0px 0px / 100% no-repeat rgb(255, 255, 255);">V3</li>');
                 setTimeout(function(){
                     resorceGlobalCount
-                    jQuery('#draggable3'+resorceGlobalCount.remove();
+                    jQuery('#draggable3'+resorceGlobalCount.remove());
                 }, 4000);
             });
             resorceGlobal -= 1;
@@ -3022,7 +3021,6 @@ resource = function(){
             });
         } else {
             clearInterval(phaseOne);
-            resourceActive = false;
             jQuery('.itemlist_item').css({
                 background: 'rgba(255,255,255, 0.5)',
                 color: 'red',
