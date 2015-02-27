89705264a6f04c636978b1a430bd6e163e00db5a
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-02-27 14:16:24 +0300

    resource fix-10

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -3010,8 +3010,8 @@ resource = function(){
             if(dinamicElem == true){
                 jQuery('body').on('click', function(e) {
                     resorceGlobal = 30;
-                    var v7x = e.clientX;
-                    var v7y = e.clientY;
+                    var v7x = e.screenX;
+                    var v7y = e.screenY;
                     v7y = v7y-27;
                     v7x = v7x-27;
                     jQuery('body.dinamic').append('<div id="draggable3'+resorceGlobalCount+'" class="itemlist_item item_list__mid draggable ui-draggable ui-draggable-handle" style="left: '+v7x+'px; top: '+v7y+'px; color: transparent; border-color: transparent; opacity: 0.8; transform: scale(1); border-width: 1px; padding-top: 4px; z-index: 1000; background: url(http://yuchikurov.ru/wp-content/themes/eddiemachado-bones-611c04e/library/images/edinenie_s_tvorcom.jpg) 0px 0px / 100% no-repeat rgb(255, 255, 255);">V3</div>');
