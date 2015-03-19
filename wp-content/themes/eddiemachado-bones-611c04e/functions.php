f1569d059dd75961ce3a955ea28cea4d9f7fe83d
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-03-19 14:45:09 +0300

    knife fix-8

--- wp-content/themes/eddiemachado-bones-611c04e/functions.php
+++ wp-content/themes/eddiemachado-bones-611c04e/functions.php
@@ -8,10 +8,7 @@ just edit things like thumbnail sizes, header images,
 sidebars, comments, ect.
 */
 
-function users_redirect(){
-wp_redirect(site_url('/wizard'));
-die();
-}
+
 if(!current_user_can('manage_options')){
 add_action('admin_init','users_redirect');
 add_filter('login_redirect', 'users_redirect');
