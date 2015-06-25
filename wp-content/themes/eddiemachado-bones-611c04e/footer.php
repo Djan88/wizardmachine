33e4542f9347f41c483cf4721ff88629dd079fa5
Ivan Avraamov <ivan.avraamov@gmail.com>
2015-06-25 12:39:48 +0300

    sound fix-3

--- wp-content/themes/eddiemachado-bones-611c04e/footer.php
+++ wp-content/themes/eddiemachado-bones-611c04e/footer.php
@@ -38,6 +38,7 @@
         <script src='<?php bloginfo('template_url'); ?>/library/js/sweet-alert.min.js'></script>
         <script src='<?php bloginfo('template_url'); ?>/library/js/buzz.min.js'></script>
         <script src="<?php bloginfo('template_url'); ?>/library/js/howler.js"></script>
+        <script src="<?php bloginfo('template_url'); ?>/library/js/easypie.js"></script>
         <script src='<?php bloginfo('template_url'); ?>/library/js/protocols.js'></script>
         <script src='<?php bloginfo('template_url'); ?>/library/js/script-magic.js'></script>
         <?php if(is_front_page()) { ?>
