cdcf93a102b62e047087c5896c9a0b8bf27a33f3
Ivan Avraamov <ivan.avraamov@gmail.com>
2016-05-13 12:24:27 +0300

    protocol fix-15

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -1954,7 +1954,7 @@
                                         top: '-120px'
                                     });
                                     jQuery('#triangle_4').css({
-                                        transform: 'scale(0.2) rotateY(0deg)rotateZ(120deg)'
+                                        transform: 'scale(0.2) rotateY(180deg)rotateZ(0deg)'
                                     });
                                     //анимация первого треугольника
                                     jQuery('#draggableS_1').addClass('transparent');
