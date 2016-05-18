66427ed87c370affb332b9504250f17a578a4fb8
Ivan Avraamov <ivan.avraamov@gmail.com>
2016-05-18 13:34:43 +0300

    protocol fix-23

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/protocols.js
@@ -993,8 +993,8 @@
                         top: '-140px'
                     });
                     //анимация первого треугольника
-                    jQuery('#draggableS_1').addClass('transparent');
-                    firstTriangleAnimation = new Vivus('triangle_1', {type: 'delayed', duration: 600}, function(){
+                    jQuery('#draggableD_1').addClass('transparent');
+                    firstTriangleAnimation = new Vivus('triangleD_1', {type: 'delayed', duration: 600}, function(){
                     //анимация второго треугольника
                         setTimeout(function(){
                             jQuery('#draggable2').addClass('transparent');
@@ -1020,8 +1020,8 @@
                                                     top: '-140px'
                                                 });
                                                 //анимация первого треугольника
-                                                jQuery('#draggableS_1').addClass('transparent');                                                                    
-                                                firstTriangleAnimation = new Vivus('triangle_1', {type: 'delayed', duration: 600}, function(){
+                                                jQuery('#draggableD_1').addClass('transparent');                                                                    
+                                                firstTriangleAnimation = new Vivus('triangleD_1', {type: 'delayed', duration: 600}, function(){
                                                 //анимация второго треугольника
                                                     setTimeout(function(){
                                                         jQuery('#draggable2').addClass('transparent');
@@ -1108,6 +1108,98 @@
                 jQuery('.itemlist_item').removeClass('transparent');
                 jQuery('.chart').data('easyPieChart').update(36);
                 jQuery('.chart').find('span').text('36');
+    //фаза 2
+                reloadTime = 0;
+                reloadTime1 = 0;
+                d12Val = 0;
+                cur_animation_val = 0;
+                rotateVal = 0;
+                count_animation = 1;
+                phaseOne = setInterval(function(){
+                    if (count_animation <= 344){                                                                         //90
+                        // tickSound.play();
+                        jQuery('#draggableD_1, #draggable3, #draggableD3').css({
+                            color: 'transparent',
+                            borderColor: 'transparent',
+                            opacity: 0.8,
+                            borderWidth: '1px',
+                            paddingTop: '4px',
+                            transform: 'rotate('+rotateVal+'deg) scale(1)',
+                            background: '#fff url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/superdisfunction.png) 0 0/100% no-repeat',
+                            zIndex: '1000'
+                        });
+                        jQuery('#draggableD12')
+                            .removeClass('hidden')
+                            .css({
+                                opacity: 0.8,
+                                transform: 'scale(1)',
+                                background: '#fff url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/lovushka.jpg) 0 0/100% no-repeat',
+                                transform: 'rotate(-'+d12Val+'deg)',
+                                borderWidth: '1px',
+                                borderColor: 'transparent'
+                            });
+                        count_animation += 1;
+                        rotateVal += 1.5;
+                        // console.log(count_animation);
+                        if(count_animation <= 120){
+                            cur_animation_val += 1.5;
+                            d12Val+= 9;
+                            jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
+                        } else if (count_animation >= 120 && count_animation <= 228){
+                            cur_animation_val -= 1.5;
+                            d12Val+= 9;
+                            jQuery('#draggableD12').css('transform', 'rotate(-'+d12Val+'deg)');
+                            jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
+                        } else if (count_animation >= 228 && count_animation <= 292){
+                            cur_animation_val -= 1.5;
+                            d12Val+= 9;
+                            jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
+                            jQuery('#draggableD12').css('transform', 'rotate('+d12Val+'deg)');
+                            jQuery('#draggableD12').css('background', '#fff url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/daemon.png) 0 0/100% no-repeat');
+                        } else if (count_animation >= 292 && count_animation <= 344){
+                            cur_animation_val += 1.5;
+                            d12Val+= 9;
+                            jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
+                            jQuery('#draggableD12').css('transform', 'rotate('+d12Val+'deg)');
+                            jQuery('#draggableD12').css('background', '#fff url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/daemon.png) 0 0/100% no-repeat');
+                        } else {
+                            d12Val+= 9;
+                            cur_animation_val += 1.5;
+                            jQuery('.box_rounded').css('transform', 'rotate('+cur_animation_val+'deg) scale(1)');
+                            jQuery('#draggableD12').css('transform', 'rotate('+d12Val+'deg)');
+                            jQuery('#draggableD12').css('background', '#fff url(/wp-content/themes/eddiemachado-bones-611c04e/library/images/daemon.png) 0 0/100% no-repeat');
+                        }
+                    } else {
+                        clearInterval(phaseOne);
+                        count_animation = 1;
+                        jQuery('#draggableD_1, #draggable3, #draggableD3').css({
+                            background: 'rgba(255,255,255, 0.5)',
+                            color: 'red',
+                            borderColor: 'red',
+                            opacity: 1,
+                            transform: 'scale(0.5)',
+                            borderWidth: '2px',
+                            paddingTop: '5px',
+                            zIndex: '1'
+                        });
+                        tickSound.stop();
+                        phaseTwo = setInterval(function(){
+                            if (reloadTime <= 1){                                                                       //1
+                                tickSound.stop();
+                                reloadSound.play();
+                                reloadTime += 1;
+                            } else {
+                                clearInterval(phaseTwo);
+                                reloadSound.stop();
+                                tickSound.play();
+                            }
+                        }, 250);
+                        jQuery('.box_rounded').css('transform', 'rotate(0deg) scale(1)');
+                        jQuery('#draggableD12').css('transform', 'rotate(0deg)');
+                        jQuery('.chart').data('easyPieChart').update(24);
+                        jQuery('.chart').find('span').text('24');
+                    }
+                }, 250);
             }
         }, 250);
     }
