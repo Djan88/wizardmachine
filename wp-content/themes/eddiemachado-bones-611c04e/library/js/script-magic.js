f13340a6dc528108b7633c794f4dd97e053f855d
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-05-20 16:31:23 +0300

    prot viscera-20

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
@@ -216,10 +216,10 @@ jQuery( ".btn__wizard" ).on('click', function(event) {
     pointsStatus = true;
     v3status = true;
     curV = localStorage.getItem('curV');
-        // checkPoints();
-        // if(pointsStatus == false){
-        //     swal("Не все зоны перенесены", "Перед началом процедуры необходимо перенести все зоны", "info"); 
-        // } else {
+        checkPoints();
+        if(pointsStatus == false){
+            swal("Не все зоны перенесены", "Перед началом процедуры необходимо перенести все зоны", "info"); 
+        } else {
             jQuery(this)
                 .addClass('btn__wizard_inAction')
                 .text('Выполняется');
@@ -252,7 +252,7 @@ jQuery( ".btn__wizard" ).on('click', function(event) {
                 } else{
                     console.log('нет протокола с id '+ protocol)
                 }
-        // }  
+        }  
     // }
     main_heading();
 });
