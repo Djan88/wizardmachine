0452e4f96faca9ff69272c296a0d13d9d74c4c91
u0352628 <u0352628@scp63.hosting.reg.ru>
2017-06-06 13:18:08 +0300

    status

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit1b66d73e8118fcc75cc4f77ad7f75459::getLoader();
+return ComposerAutoloaderInit705a1972ae70196fe3c15874d70a0ac9::getLoader();
