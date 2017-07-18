a03e966aba699f6536157fa17b622711774125a6
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-07-18 13:17:21 +0300

    home fix-12

--- wp-content/themes/eddiemachado-bones-611c04e/footer.php
+++ wp-content/themes/eddiemachado-bones-611c04e/footer.php
@@ -36,7 +36,46 @@
                             <h4 class="modal-title" id="myModalLabel">Название модали</h4>
                         </div>
                         <div class="modal-body">
-                            ...
+                            <div class="btn btn_lg btn_trans show_form">Вход для сертифицированных пользователей</div><br>
+                            <a href="/wp-content/uploads/2015/02/Kniga_po_Vizard-terapii.compressed.pdf" target="_blank" class="button button-primary button-large button__main">Книга "Визард терапия"</a>
+                            <a href="/wp-content/uploads/2015/02/Osnovy_biologicheskogo_tsentrirovania.compressed.pdf" target="_blank" class="button button-primary button-large button__main">Книга "Биологическое центрирование"</a>
+                            <a href="https://www.youtube.com/watch?v=ZZPjJGX-BM4" target="_blank" class="button button-primary button-large button__main">Видео</a>
+                            <a href="http://wizardduos.ru/" target="_blank" class="button button-primary button-large button__main">"Wizard Duos"</a></br>
+                            <a href="https://vk.com/id139677998" target="_blank" class="button button-primary button-large button__main button_social button_social_vk">vk</a>
+                            <div class="login__form">
+                                <form name="loginform" id="loginform" action="<?php echo esc_url( site_url( 'wp-login.php', 'login_post' ) ); ?>" method="post">
+                                    <p>
+                                        <label for="user_login"><?php _e('Username') ?><br />
+                                            <input type="text" name="log" id="user_login" class="input" value="<?php echo esc_attr($user_login); ?>" size="20" /></label>
+                                    </p>
+                                    <p>
+                                        <label for="user_pass"><?php _e('Password') ?><br />
+                                            <input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+                                    </p>
+                                    <?php
+                                    /**
+                                     * Fires following the 'Password' field in the login form.
+                                     *
+                                     * @since 2.1.0
+                                     */
+                                    do_action( 'login_form' );
+                                    ?>
+                                    <p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever" <?php checked( $rememberme ); ?> /> <?php esc_attr_e('Remember Me'); ?></label></p>
+                                    <p class="note_small">Для получения сертификата пользователя необходимо пройти очное либо дистантное обучение. <a class="bablosadres" href="mailto:info@chikurov.com" style="color: #fff;">info@chikurov.com</a></p>
+                                    <p class="submit">
+                                        <input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="<?php esc_attr_e('Log In'); ?>" />
+                                        <?php   if ( $interim_login ) { ?>
+                                            <input type="hidden" name="interim-login" value="1" />
+                                        <?php   } else { ?>
+                                            <input type="hidden" name="redirect_to" value="<?php echo esc_attr($redirect_to); ?>" />
+                                        <?php   } ?>
+                                        <?php   if ( $customize_login ) : ?>
+                                            <input type="hidden" name="customize-login" value="1" />
+                                        <?php   endif; ?>
+                                        <input type="hidden" name="testcookie" value="1" />
+                                    </p>
+                                </form>
+                            </div>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>
