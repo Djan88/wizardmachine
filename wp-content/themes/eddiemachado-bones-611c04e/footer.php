d7e344d8d32c29051df182d49e7634dce206eeee
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-07-31 12:20:36 +0300

    home fix-38

--- wp-content/themes/eddiemachado-bones-611c04e/footer.php
+++ wp-content/themes/eddiemachado-bones-611c04e/footer.php
@@ -82,7 +82,7 @@
             </div>
             <!-- Modal two -->
             <div class="modal fade" id="myModal_choice" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
+                <div class="modal-dialog modal-sm">
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
