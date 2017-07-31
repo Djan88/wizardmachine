e07c00d384dbf6f07212dae9978b1c7a2ae7fe29
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-07-31 12:25:23 +0300

    home fix-40

--- wp-content/themes/eddiemachado-bones-611c04e/footer.php
+++ wp-content/themes/eddiemachado-bones-611c04e/footer.php
@@ -36,7 +36,7 @@
                             <h4 class="modal-title" id="myModalLabel">Вход для сертифицированных пользователей</h4>
                         </div>
                         <div class="modal-body">
-                            <a href="/wp-content/uploads/2015/02/Kniga_po_Vizard-terapii.compressed.pdf" target="_blank" class="button button-primary button-large button__main">Книга "Визард терапия"</a>
+                            <a href="www.wizardduos.ru" target="_blank" class="button button-primary button-large button__main">"Wizard Duos"</a>
                             <a href="/wp-content/uploads/2015/02/Osnovy_biologicheskogo_tsentrirovania.compressed.pdf" target="_blank" class="button button-primary button-large button__main">Книга "Биологическое центрирование"</a>
                             <a href="https://www.youtube.com/watch?v=ZZPjJGX-BM4" target="_blank" class="button button-primary button-large button__main">Видео</a>
                             <div class="login__form">
