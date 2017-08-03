7c4182bc04a1adf3aaebba2dda42fc45520fb2c4
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-08-03 19:24:26 +0300

    fix-11

--- disc/script.js
+++ disc/script.js
@@ -1,13 +1,12 @@
 jQuery(document).ready(function() {
   var valueNow,
-      valueNow_,
       highlighter,
       tickSound = new buzz.sound( "seif", {
           formats: [ "ogg", "mp3" ]
       });
   $("#handle1").roundSlider({
     min: 0,
-    max: 120000,
+    max: 12000,
     step: 1000,
     value: 0,
     radius: 200,
@@ -18,30 +17,29 @@ jQuery(document).ready(function() {
   });
   highlighter = function(){
     tickSound.play();
-    console.log(valueNow_);
     jQuery('.elem_pos').each(function(index, el) {
       jQuery(this).removeClass('elem_pos_active')
     });
-    if (valueNow == 'z-index: 8; transform: rotate(0deg);' || valueNow == 'z-index: 8; transform: rotate(120deg);' || valueNow == 'z-index: 8; transform: rotate(240deg);' || valueNow == 'z-index: 8; transform: rotate(360deg);') {
+    if (valueNow == 0 || valueNow == 4000 || valueNow == 8000 || valueNow == 12000) {
       jQuery('.elem_pos_d').each(function(index, el) {
         jQuery(this).addClass('elem_pos_active');
       });
-    } else if (valueNow == 'z-index: 8; transform: rotate(30deg);' || valueNow == 'z-index: 8; transform: rotate(150deg);' || valueNow == 'z-index: 8; transform: rotate(270deg);') {
+    } else if (valueNow == 1000 || valueNow == 5000 || valueNow == 9000) {
       jQuery('.elem_pos_t').each(function(index, el) {
         jQuery(this).addClass('elem_pos_active');
       });
-    }  else if (valueNow == 'z-index: 8; transform: rotate(60deg);' || valueNow == 'z-index: 8; transform: rotate(180deg);' || valueNow == 'z-index: 8; transform: rotate(300deg);') {
+    }  else if (valueNow == 2000 || valueNow == 6000 || valueNow == 10000) {
       jQuery('.elem_pos_r').each(function(index, el) {
         jQuery(this).addClass('elem_pos_active');
       });
-    }   else if (valueNow == 'z-index: 8; transform: rotate(90deg);' || valueNow == 'z-index: 8; transform: rotate(210deg);' || valueNow == 'z-index: 8; transform: rotate(330deg);') {
+    }   else if (valueNow == 3000 || valueNow == 7000 || valueNow == 11000) {
       jQuery('.elem_pos_s').each(function(index, el) {
         jQuery(this).addClass('elem_pos_active');
       });
     } 
   }
   $("#handle1").on("drag", function (e) {
-      valueNow = jQuery('.rs-bar').attr('style');
+      valueNow = jQuery('.rs-handle').attr('aria-valuenow');
       highlighter();
   })
 });
