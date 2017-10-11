c323cebf93fb33ee35420574f88ebdf2fc9f9d56
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-10-11 16:54:13 +0300

    knife-21

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
@@ -41,9 +41,13 @@ jQuery(function() {
             knife_rate_class = 'knife_rate-'+knife;
             knife_rate_class_dotted = '.knife_rate-'+knife;
             jQuery('.knife-wrap').append('<div class='+knife_rate_class+'></div>');
-            jQuery(knife_rate_class_dotted).addClass('knife_rate').css({
-                top: knife
-            });
+            if(jQuery(knife_rate_class_dotted).length){
+                console.log(knife_rate_class_dotted);
+            } else {
+                jQuery(knife_rate_class_dotted).addClass('knife_rate').css({
+                    top: knife
+                });
+            }
             knifeDateOld = knifeDate;
         }
     });
