b2262df6919d310e8ff0e25168b3a1da3149375b
Ivan Avraamov <i.avraamov@yandex.ru>
2017-10-11 13:31:52 +0300

    knife

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit0935e3cc2cd2930df80d2048a010631e::getLoader();
+return ComposerAutoloaderInit2da3cc58521ce09b8a6716ef9756e74c::getLoader();
