430cf049b011be77ec1f28c06155c4d9aeabbb83
Ivan Avraamov <ivan.avraamov@gmail.com>
2017-11-27 12:04:19 +0300

    status

--- wp-content/themes/eddiemachado-bones-611c04e/footer.php
+++ wp-content/themes/eddiemachado-bones-611c04e/footer.php
@@ -94,7 +94,6 @@
                         <div class="modal-footer">
                             <a href="http://help.yandex.ru/common/support/screenshot.xml" target="_blank" class="button button-primary button-large button__main btn_choice_screen">Как сделать "скриншот" экрана</a>
                             <a href="/wp-content/uploads/2015/02/Manual_dlya_Wizardmachine.pdf" target="_blank" class="button button-primary button-large button__main btn_choice_reference">Руководство пользователя</a>
-                            <a href="www.wizardduos.ru" target="_blank" class="button button-primary button-large button__main">"Wizard Duos"</a>
                             <a href="/wp-content/uploads/2015/02/Osnovy_biologicheskogo_tsentrirovania.compressed.pdf" target="_blank" class="button button-primary button-large button__main">Книга "Биологическое центрирование"</a>
                             <a href="https://www.youtube.com/watch?v=ZZPjJGX-BM4" target="_blank" class="button button-primary button-large button__main">Учебное видео</a>
                             <!-- <div><a href="/pay/" target="_blank" class="btn btn_lg btn_trans" style="margin-top: 10px; margin-left: 15px;">Оплата доступа</a></div> -->
