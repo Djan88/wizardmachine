6380dae2abafaa6bbcee123bf9c91c84ea8f24ef
Ivan Avraamov <i.avraamov@yandex.ru>
2017-12-11 16:32:59 +0300

    status

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitbc42a58bc348cc5d6aeb1496135741cb::getLoader();
+return ComposerAutoloaderInit8a8357a29a59592583f0693c224590f6::getLoader();
