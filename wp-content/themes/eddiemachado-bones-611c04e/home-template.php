0eafbafec602872935a1bb50bc6d0613237f7467
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-04-24 14:18:14 +0300

    home

--- wp-content/themes/eddiemachado-bones-611c04e/home-template.php
+++ wp-content/themes/eddiemachado-bones-611c04e/home-template.php
@@ -10,9 +10,31 @@
     <div id="inner-content" class="wrap cf">
         <div id="main" class="m-all t-2of3 cf <?php if(current_user_can('subscriber')){ ?>subscriber_wrap<?php } ?>" role="main">
             <div class="home-page-content <?php if(current_user_can('subscriber')){ ?>subscriber<?php } ?>">    
-                <?php if(is_user_logged_in()){ ?>
-                    <!-- Если зашел подписчик -->
-                    <?php if(current_user_can('subscriber')){ ?>
+                <?php if(current_user_can('contributor') || current_user_can('administrator')) { ?>
+                        
+                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        <div class="reg_block">
+                          <?php 
+                            $user = get_current_user_id();
+                            $cur_user_data = get_userdata($user);
+                            $user_reg = $cur_user_data->get('user_registered');
+                            $user_login = $cur_user_data->get('user_login');
+                            $user_mail = $cur_user_data->get('user_email');
+                            $year_val = 31536000;
+                            $regtime = strtotime($user_reg);
+                            $cur_data = time();
+                            $ratio =($cur_data - $regtime) - $year_val;
+                            $ratioten = ($cur_data - $regtime) - $year_val + 864000;
+                          ?>
+                          <?php if ($ratioten > 0) { ?>
+                            <div class="ratioten" data-ratio="<?php echo $ratioten;?>"></div>
+                          <?php } ?>
+                        </div>
+                        <?php endwhile; ?>
+                        <?php endif; ?> 
+                    <?php } ?>
+                <?php } else { ?>
+                    <div class="home-content" style="text-align: right">
                         <div class="col-md-12">
                           <h1 class="vitrin_heading">Интерактивный веб-проект "WizardMachine"</h1>
                           <div class="row">
@@ -93,33 +115,6 @@
                                 </p>
                             </div>
                         </footer>
-                    <!-- Если зашел участник или администратор -->
-                    <?php } elseif(current_user_can('contributor') || current_user_can('administrator')) { ?>
-                        
-                        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                        <div class="reg_block">
-                          <?php 
-                            $user = get_current_user_id();
-                            $cur_user_data = get_userdata($user);
-                            $user_reg = $cur_user_data->get('user_registered');
-                            $user_login = $cur_user_data->get('user_login');
-                            $user_mail = $cur_user_data->get('user_email');
-                            $year_val = 31536000;
-                            $regtime = strtotime($user_reg);
-                            $cur_data = time();
-                            $ratio =($cur_data - $regtime) - $year_val;
-                            $ratioten = ($cur_data - $regtime) - $year_val + 864000;
-                          ?>
-                          <?php if ($ratioten > 0) { ?>
-                            <div class="ratioten" data-ratio="<?php echo $ratioten;?>"></div>
-                          <?php } ?>
-                        </div>
-                        <?php endwhile; ?>
-                        <?php endif; ?> 
-                    <?php } ?>
-                <?php } else { ?>
-                    <div class="home-content" style="text-align: right">
-<!--                        <h2 class="home_heading" data-toggle="modal" data-target="#myModal_login">WIZARDMACHINE</h2>-->
                     </div>
                 <?php } ?>
             </div>
