6c1abf305860f037e9d9a7b8ee8cb34f40bf278a
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-04-28 13:43:13 +0300

    cabinet

--- wp-content/themes/eddiemachado-bones-611c04e/functions.php
+++ wp-content/themes/eddiemachado-bones-611c04e/functions.php
@@ -8,14 +8,6 @@ just edit things like thumbnail sizes, header images,
 sidebars, comments, ect.
 */
 
-function users_redirect(){
-wp_redirect(site_url('/'));
-die();
-}
-if(!current_user_can('manage_options')){
-add_action('admin_init','users_redirect');
-add_filter('login_redirect', 'users_redirect');
-}
 
 // LOAD BONES CORE (if you remove this, the theme will break)
 require_once( 'library/bones.php' );
