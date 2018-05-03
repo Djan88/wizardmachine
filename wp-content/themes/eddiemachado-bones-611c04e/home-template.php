500017a7663ce6428545f3dec5ab982ccf346b9e
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-05-03 21:19:21 +0300

    cabinet-34

--- wp-content/themes/eddiemachado-bones-611c04e/home-template.php
+++ wp-content/themes/eddiemachado-bones-611c04e/home-template.php
@@ -50,7 +50,7 @@
             <h2>Как приобрести доступ к "WizardMachine"?</h2>
             <div class="row">
                 <div class="col-md-10 col-md-offset-1">
-                    <div class="vitrin vitrin-content">
+                    <div class="vitrin vitrin-content vitrin-centered">
                         Стоимость получения доступа к программе на 1 год составляет <div class="vitrin-price">35 000 руб.</div>. Через год и далее Вы сможете продлить Вашу учетную запись за <div class="vitrin-price">25 000 руб.</div>
                     </div>
                     <div class="vitrin vitrin-content">
