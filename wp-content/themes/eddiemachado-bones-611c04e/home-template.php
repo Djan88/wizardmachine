6f4129003389787cdd2ca089efd872262f9af1ff
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-05-03 21:27:19 +0300

    cabinet-38

--- wp-content/themes/eddiemachado-bones-611c04e/home-template.php
+++ wp-content/themes/eddiemachado-bones-611c04e/home-template.php
@@ -53,7 +53,7 @@
                     <div class="vitrin vitrin-content vitrin-centered">
                         Стоимость получения доступа к программе на 1 год составляет <div class="vitrin-price">35 000 руб.</div> Через год и далее Вы сможете продлить Вашу учетную запись за <div class="vitrin-price">25 000 руб.</div>
                     </div>
-                    <div class="vitrin vitrin-content">
+                    <div class="vitrin vitrin-content vitrin-pay">
                         <b>Чтоб получить доступ к программе необходимо сделать следующее:</b>
                     
                         <p>
