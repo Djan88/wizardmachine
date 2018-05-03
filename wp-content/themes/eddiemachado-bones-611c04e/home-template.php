84127e6422d2675de9e6504f4f7d86863665e712
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-05-03 21:22:37 +0300

    cabinet-36

--- wp-content/themes/eddiemachado-bones-611c04e/home-template.php
+++ wp-content/themes/eddiemachado-bones-611c04e/home-template.php
@@ -54,19 +54,20 @@
                         Стоимость получения доступа к программе на 1 год составляет <div class="vitrin-price">35 000 руб.</div> Через год и далее Вы сможете продлить Вашу учетную запись за <div class="vitrin-price">25 000 руб.</div>
                     </div>
                     <div class="vitrin vitrin-content">
-                        Чтоб получить доступ к программе необходимо сделать следующее:
-                    </div>
-                    <div class="vitrin vitrin-content">
-                        <span class="article_main">— </span><span class="reg_login toLogin" data-toggle="modal" data-target="#myModal_login">Войдите</span> на сайт. Если у Вас еще нет учетной записи "WizardMachine" <span class="reg_login toRegistration" data-toggle="modal" data-target="#myModal_login">зарегистрируйтесь</span>.
-                    </div>
-                    <div class="vitrin vitrin-content">
-                        <span class="article_main">— </span> Войдите в <a href="/kabinet">личный кабинет</a> пользователя.
-                    </div>
-                    <div class="vitrin vitrin-content">
-                        <span class="article_main">— </span> Выберите вкладку <b>"Все доступы"</b>, нажмите "Оплатить" -> "Оплатить через "Робокассу" -> Выберите удобный способ и произведите оплату. (Напомниаем что начиная со второго года пользователи "WizardMachine" получают скидку на продление лицензии 30%)
-                    </div>
-                    <div class="vitrin vitrin-content">
-                        По завершении оплаты на Вашу почту поступит письмо с подтверждением. С этой минуты Ваш доступ к программе "WizardMachine" активен! Приятного пользования!
+                        <b>Чтоб получить доступ к программе необходимо сделать следующее:</b>
+                    
+                        <p>
+                            <span class="article_main">— </span><span class="reg_login toLogin" data-toggle="modal" data-target="#myModal_login">Войдите</span> на сайт. Если у Вас еще нет учетной записи "WizardMachine" <span class="reg_login toRegistration" data-toggle="modal" data-target="#myModal_login">зарегистрируйтесь</span>.
+                        </p>
+                        <p>
+                            <span class="article_main">— </span> Войдите в <a href="/kabinet">личный кабинет</a> пользователя.
+                        </p>
+                        <p>
+                            <span class="article_main">— </span> Выберите вкладку <b>"Все доступы"</b>, нажмите "Оплатить" -> "Оплатить через "Робокассу" -> Выберите удобный способ и произведите оплату. (Напомниаем что начиная со второго года пользователи "WizardMachine" получают скидку на продление лицензии 30%)
+                        </p>
+                        <p>
+                            По завершении оплаты на Вашу почту поступит письмо с подтверждением. С этой минуты Ваш доступ к программе "WizardMachine" активен! Приятного пользования!
+                        </p>
                     </div>
                     <div class="vitrin vitrin-content vitrin-centered">
                         Если у Вас возникли вопросы напишите на <a href="mailto:info@chikurov.com">info@chikurov.com</a>. Вам оперативно ответят!
