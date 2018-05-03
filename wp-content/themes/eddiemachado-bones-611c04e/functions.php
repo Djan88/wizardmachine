8ed0eed1275ba639516b12b092f762305faa3277
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-05-03 18:29:37 +0300

    cabinet-27

--- wp-content/themes/eddiemachado-bones-611c04e/functions.php
+++ wp-content/themes/eddiemachado-bones-611c04e/functions.php
@@ -251,23 +251,6 @@ if (!current_user_can('administrator')):
 endif;
 
 
-/* редирект с login на /wp-login.php  и с admin на /wp-admin */
-add_action('template_redirect', 'kama_login_redirect');
-function kama_login_redirect(){
-    if(!is_user_logged_in()){
-  if( strpos($_SERVER['REQUEST_URI'], 'login')!==false )
-    $loc = '/wp-login.php';
-  elseif( strpos($_SERVER['REQUEST_URI'], 'admin')!==false )
-    $loc = '/wp-admin/';
-        elseif( strpos($_SERVER['REQUEST_URI'], 'registration')!==false )
-    $loc = 'wp-login.php?action=register';
-  if( $loc ){
-    header( 'Location: '.get_option('site_url').$loc, true, 303 );
-    exit;
-  }
-    }
-}
-
 add_filter("login_redirect", "sp_login_redirect", 10, 3);
 
 function sp_login_redirect($redirect_to, $request, $user){
