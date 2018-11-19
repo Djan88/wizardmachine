71597d3d5a1e6e4004b5f45e5c1cc25cddafb043
Ivan Avraamov <ivan.avraamov@gmail.com>
2018-11-19 11:43:56 +0300

    modals-12

--- wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
+++ wp-content/themes/eddiemachado-bones-611c04e/library/js/script-magic.js
@@ -116,10 +116,10 @@ jQuery(function() {
     if(supportsStorage && localStorage.getItem('popupStatus1')){
       popupStatus1 = localStorage.getItem('popupStatus1');
       cur_time = new Date().getTime();
-      console.log(popupStatus1);
-      console.log(cur_time);
-      console.log(popupStatus1 + 8000 - cur_time);
     }
+    console.log(popupStatus1);
+    console.log(cur_time);
+    console.log(cur_time - (popupStatus1 + 8000));
     if (popupStatus1 && popupStatus1+8000 < cur_time) {
       setTimeout(openPopupOne, 5000);
       // jQuery('#book_down').on('hidden.bs.modal', function (e) {
