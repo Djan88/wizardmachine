48010391e68d3f8803516e9bc4aad3a8532c5235
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-10-07 19:17:20 +0300

    new 5 ver-4

--- wp-content/themes/bcwish/js/script.js
+++ wp-content/themes/bcwish/js/script.js
@@ -70,7 +70,7 @@ jQuery(function() {
   });
 
 
-  jQuery('.mobile-nav-toggle, .mobile-nav a, .photo_upload, .crop_photo, .btn_diag, .btn_prot_choice, .wizard_clean_graf, .btn_prot_choice_fromDiag, #faq-list li, .wizard_protocol, .wizard_play, .wizard_stop, body .cancel, body .confirm, .wizard_continue').on('click', function(event) {
+  jQuery('.mobile-nav-toggle, .mobile-nav a, .photo_upload, .crop_photo, .btn_diag, .btn_prot_choice, .wizard_clean_graf, .btn_prot_choice_fromDiag, #faq-list li, .wizard_protocol, .wizard_play, .wizard_stop, body .cancel, body .confirm, .wizard_continue, .mobile-nav select').on('click', function(event) {
     nextSound.play();
   });
   jQuery('.wizard_to_protList, .wizard_to_what_way, .wizard_to_start').on('click', function(event) {
