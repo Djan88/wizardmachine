02a3124f0c44be0e63532776fa11a2144968318d
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-10-11 14:42:20 +0300

    theme-38

--- wp-content/themes/bcwish/js/protocols.js
+++ wp-content/themes/bcwish/js/protocols.js
@@ -155,7 +155,7 @@ jQuery(function() {
         knife_rate_class_dotted = '.knife_rate-'+knife;
         jQuery('.wizard_grafic').append('<div class='+knife_rate_class+'></div>');
         jQuery(knife_rate_class_dotted).addClass('knife_rate').css({
-            top: +knife+10+'px',
+            top: +knife+15+'px',
             width: knifeDateDiff*2+'px'
         });
         knifeDateOld = knifeDate;
