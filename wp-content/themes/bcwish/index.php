738f7fed77f49e6774e7633747508e09a0617d3f
Ivan Avraamov <ivan.avraamov@gmail.com>
2019-11-01 12:43:28 +0300

    dots-7

--- wp-content/themes/bcwish/index.php
+++ wp-content/themes/bcwish/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title><?php wp_title(''); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+  <meta name="viewport" content="user-scalable=no" />
   <meta content="" name="keywords">
   <meta content="" name="description">
 
