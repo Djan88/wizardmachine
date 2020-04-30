d30231813e4c7274e301da1e6c148bf5c391dfe6
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-04-30 16:39:25 +0300

    wizard_test-2

--- wp-content/themes/bcwish/js/protocols.js
+++ wp-content/themes/bcwish/js/protocols.js
@@ -1403,7 +1403,7 @@ jQuery(function() {
     rotateVal = 0;
     count_animation = 1;
     phaseOne = setInterval(function(){
-      if (count_animation <= 344){
+      if (count_animation <= 86){
         if (reloadTime == 0){                                                                       //1
             sound.stop();
             reloadSound.play();
@@ -1420,15 +1420,15 @@ jQuery(function() {
             transform: 'scale(1.5)',
             zIndex: '1000'
         });
-        if (count_animation > 0 && count_animation <= 120) {
+        if (count_animation > 0 && count_animation <= 30) {
           jQuery('.zone_v2, .zone_d2_, .zone_d3, .zone_d4').css({
             background: '#fff url(/wp-content/themes/bcwish/img/disfunction.png) center center/100% no-repeat'
           });
-        } else if (count_animation > 120 && count_animation <= 220) {
+        } else if (count_animation > 30 && count_animation <= 55) {
           jQuery('.zone_v2, .zone_d2_, .zone_d3, .zone_d4').css({
             background: '#fff url(/wp-content/themes/bcwish/img/travma.png) center center/100% no-repeat'
           });
-        } else if (count_animation > 220) {
+        } else if (count_animation > 55) {
           jQuery('.zone_v2, .zone_d2_, .zone_d3, .zone_d4').css({
             background: '#fff url(/wp-content/themes/bcwish/img/povregdenie_demona.png) center center/100% no-repeat'
           });
@@ -1452,37 +1452,35 @@ jQuery(function() {
             transform: 'rotate(-'+d12Val+'deg) scale(1.5)'
           });
         count_animation += 1;
-        rotateVal += 2;
-        if(count_animation <= 120){
-            cur_animation_val += 2;
+        rotateVal += 1.5;
+        if(count_animation <= 30){
+            cur_animation_val += 1.5;
             d12Val+= 3;
             jQuery('.ring').css('transform', 'rotate('+cur_animation_val+'deg)');
-        } else if (count_animation >= 120 && count_animation <= 228){
-            cur_animation_val -= 2;
+        } else if (count_animation >= 30 && count_animation <= 57){
+            cur_animation_val -= 1.5;
             d12Val+= 3;
             jQuery('.zone_ring').css('transform', 'rotate(-'+d12Val+'deg) scale(1.5)');
             jQuery('.ring').css('transform', 'rotate('+cur_animation_val+'deg)');
-        } else if (count_animation >= 228 && count_animation <= 292){
-            cur_animation_val -= 2;
+        } else if (count_animation >= 57 && count_animation <= 73){
+            cur_animation_val -= 1.5;
             d12Val+= 3;
             jQuery('.ring').css('transform', 'rotate('+cur_animation_val+'deg)');
             jQuery('.zone_ring').css('transform', 'rotate('+d12Val+'deg) scale(1.5)');
             jQuery('.zone_ring').css('background', '#fff url(/wp-content/themes/bcwish/img/daemon.png) center center/100% no-repeat');
-        } else if (count_animation >= 292 && count_animation <= 344){
-            cur_animation_val += 2;
+        } else if (count_animation >= 73 && count_animation <= 86){
+            cur_animation_val += 1.5;
             d12Val+= 3;
             jQuery('.ring').css('transform', 'rotate('+cur_animation_val+'deg)');
             jQuery('.zone_ring').css('transform', 'rotate('+d12Val+'deg) scale(1.5)');
             jQuery('.zone_ring').css('background', '#fff url(/wp-content/themes/bcwish/img/daemon.png) center center/100% no-repeat');
         } else {
             d12Val+= 3;
-            cur_animation_val += 2;
+            cur_animation_val += 1.5;
             jQuery('.ring').css('transform', 'rotate('+cur_animation_val+'deg)');
             jQuery('.zone_ring').css('transform', 'rotate('+d12Val+'deg) scale(1.5)');
             jQuery('.zone_ring').css('background', '#fff url(/wp-content/themes/bcwish/img/daemon.png) center center/100% no-repeat');
         }
-        console.log(cur_animation_val);
-        console.log(d12Val);
       } else {
         clearInterval(phaseOne);
         count_animation = 1;
@@ -1505,7 +1503,7 @@ jQuery(function() {
           // console.log('continue');
         } 
       }
-    }, 250);
+    }, 1000);
   }
 
   drenag_12 = function(){
