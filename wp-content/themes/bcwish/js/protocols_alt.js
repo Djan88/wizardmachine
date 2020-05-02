b59d93e80743a294269d5c67e5c65022c13cf6d9
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-05-02 13:08:25 +0300

    wizard_test-38

--- wp-content/themes/bcwish/js/protocols_alt.js
+++ wp-content/themes/bcwish/js/protocols_alt.js
@@ -1083,14 +1083,14 @@ jQuery(function() {
              .fromTo('.ring',{rotation: 180}, {duration: 30, ease: "none", rotation: 0})
              .fromTo('.ring',{rotation: 0}, {duration: 15, ease: "none", rotation: -90})
              .fromTo('.ring',{rotation: -90}, {duration: 15, ease: "none", rotation: 0})
-             .to('.zone_cl', {duration: 11, ease: "none", scale: 1})
+             .to('.zone_cl', {duration: 1, ease: "none", scale: 1})
 
     var zone_ring_gsap = gsap.timeline();
     zone_ring_gsap.fromTo('.zone_ring',{rotation: 0}, {duration: 60, ease: "none", rotation: -480})
                   .fromTo('.zone_ring',{rotation: -480}, {duration: 30, ease: "none", rotation: -240})
 
     phaseOne = setInterval(function(){
-      if (count_animation <= 360){
+      if (count_animation <= 361){
         if (reloadTime == 0){                                                                       //1
             sound.stop();
             reloadSound.play();
