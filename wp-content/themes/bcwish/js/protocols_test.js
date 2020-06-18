58afaefcd6403a96438f647cd892c7b12057f1df
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-06-18 14:01:24 +0300

    keyframes

---
+++ wp-content/themes/bcwish/js/protocols_test.js
@@ -0,0 +1,359 @@
+jQuery(function() {
+  var knife,
+        knifeDate,
+        knifeDateOld,
+        knifeDateDiff,
+        knife_rate_class,
+        knife_rate_class_dotted,
+        protocol,
+        checkPoints,
+        pointsStatus = true,
+        reloadTime = 0,
+        reloadTime1 = 0,
+        d12Val = 0,
+        cur_animation_val = 0,
+        rotateVal = 0,
+        count_animation = 1,
+        pausedStatus = false,
+        endNow,
+        onEnd,
+        protocolfromMemory,
+        firstTriangleAnimation,
+        secondTriangleAnimation,
+        thirdTriangleAnimation,
+        fourthTriangleAnimation,
+        // sound = new buzz.sound( "/sounds/tick", {
+        //     formats: [ "ogg", "mp3" ]
+        // }),
+        // reloadSound = new buzz.sound( "/sounds/reload", {
+        //     formats: [ "ogg", "mp3" ]
+        // }),
+        sound = new Howl({
+            urls: ['/sounds/tick.ogg', '/sounds/tick.mp3'],
+            autoplay: false,
+            loop: false,
+            buffer: true
+        }),
+        reloadSound = new Howl({
+            urls: ['/sounds/complete.mp3'],
+            autoplay: false,
+            loop: false,
+            buffer: true
+        }),
+        alertSound = new Howl({
+            urls: ['/sounds/success.mp3'],
+            autoplay: false,
+            loop: false,
+            buffer: true
+        }),
+        alert_altSound = new Howl({
+            urls: ['/sounds/alert_alt.mp3'],
+            autoplay: false,
+            loop: false,
+            buffer: true
+        }),
+        supportsStorage = function(){
+            try {
+                return 'localStorage' in window && window['localStorage'] !== null;
+            } catch (e) {
+                return false;
+            }
+        };
+        // jQuery('.ring, .zone_ring').css('transform', 'rotate(0deg)').removeClass('hidden');
+
+    //Изменение размера круга
+    jQuery('#ring').resizable({
+      aspectRatio: 1/1
+    });
+
+    endNow = function(){
+      reloadTime = 0;
+      reloadTime1 = 0;
+      d12Val = 0;
+      cur_animation_val = 0;
+      rotateVal = 0;
+      count_animation = 1;
+      alertSound.play();
+      jQuery('.ring').removeClass('in_progress');
+
+      swal({
+        title: "Приостановлено пользователем",
+        text: "Что делать дальше?",
+        type: "info",
+        showCancelButton: true,
+        confirmButtonClass: "btn-danger",
+        cancelButtonClass: "btn-success",
+        cancelButtonText: "Продолжить",
+        confirmButtonText: "К началу",
+        closeOnConfirm: false
+      },
+      function(isConfirm) {
+        if (isConfirm) {
+          jQuery(location).attr('href','/');
+        } else {
+          jQuery('.wizard_stop, .zone_ring').addClass('hidden');
+          jQuery('.wizard_play, .wizard_starter_alt').fadeIn(500).removeClass('hidden');
+        }
+      })
+    }
+
+    onEnd = function(){
+      jQuery('.ring').removeClass('in_progress');
+      jQuery('.btn-to_endNow').addClass('hidden');
+      jQuery('.btn_start').removeAttr('disabled');
+      jQuery('.wizard_percent').text('100%');
+      rotate_one = 0;
+      rotate_two = 0;
+      rotate_three = 0;
+      rotate_four = 0;
+      rotate_lovushka = 0;
+      count_animation = 0;
+      localStorage.removeItem('paused');
+      localStorage.removeItem('pausedPhoto');
+      pausedStatus = false;
+
+      // protocolName = localStor
+      alertSound.play();
+      swal({
+        title: "Протокол завершен",
+        text: "Что делать дальше?",
+        type: "success",
+        showCancelButton: true,
+        confirmButtonClass: "btn-danger",
+        cancelButtonClass: "btn-success",
+        cancelButtonText: "Другой протокол",
+        confirmButtonText: "Новый клиент",
+        closeOnConfirm: false
+      },
+      function(isConfirm) {
+        if (isConfirm) {
+          jQuery(location).attr('href','/');
+        } else {
+          jQuery('.wizard_main_screen, .wizard_to_protList').addClass('hidden');
+          jQuery('.wizard_stop').addClass('hidden');
+          jQuery('.wizard_prots, .wizard_operation, .wizard_to_what_way').fadeIn(500).removeClass('hidden');
+          jQuery('.wizard_heading').text('Выберите протокол');
+        }
+      })
+    }
+
+    //Dragging elems
+    jQuery('.draggable, .ring').draggable({
+      snap: false
+    });
+
+    // Dragging knife
+    jQuery('.marakata').draggable({
+      containment: '.wizard_grafic',
+      axis: 'y',
+      drag: function() {
+        // if(jQuery('.btn_graf').hasClass('active')){
+          jQuery('.wizard_clean_graf').fadeIn(500).removeClass('hidden');
+          knife = jQuery('.marakata').css('top');
+          knife = knife.substr(0, knife.length - 2);
+          knifeDate = new Date();
+          knifeDateDiff = knifeDate - knifeDateOld;
+          knife_rate_class = 'knife_rate-'+knife;
+          knife_rate_class_dotted = '.knife_rate-'+knife;
+          jQuery('.wizard_grafic').append('<div class='+knife_rate_class+'></div>');
+          jQuery(knife_rate_class_dotted).addClass('knife_rate').css({
+              top: +knife+15+'px',
+              width: knifeDateDiff*2+'px'
+          });
+          knifeDateOld = knifeDate;
+        // }
+      }
+    });
+
+  universal = function(){
+    jQuery('.wizard_heading').text('Выполняется Универсальный протокол');
+    jQuery('.wizard_percent').text('0%');
+    reloadTime = 0;
+    cur_animation_val = 0;
+    count_animation = 1;
+    jQuery('.zone_v2, .zone_d2_, .zone_d3, .zone_d4, .zone_cl').css({
+        color: 'transparent',
+        borderColor: 'transparent',
+        opacity: 0.8,
+        borderWidth: '1px',
+        paddingTop: '4px',
+        transform: 'rotate(0deg) scale(1.5)',
+        zIndex: '1000'
+    });
+    jQuery('.zone_ring')
+      .removeClass('hidden')
+      .css({
+        opacity: 0.8,
+        transform: 'scale(1.5)',
+        background: '#fff url(/wp-content/themes/bcwish/img/lovushka.png) center center/100% no-repeat'
+      });
+    jQuery('.zone_cl').addClass('rot_90_one').css({
+      background: '#fff url(/wp-content/themes/bcwish/img/lovushka.png) center center/100% no-repeat',
+    });
+    jQuery('.ring').addClass('rot_ring');
+    // jQuery('.zone_ring').addClass('rot_zone_ring');
+    // gsap.fromTo(".zone_cl",{scale: 1.5, rotation: 0}, {duration: 90, ease: "none", rotation: -360, scale: 1.5});
+
+
+
+    // var zone_gsap = gsap.timeline();
+    // zone_gsap.fromTo('.ring',{rotation: 0}, {duration: 30, ease: "none", rotation: 180})
+    //          .fromTo('.ring',{rotation: 180}, {duration: 30, ease: "none", rotation: 0})
+    //          .fromTo('.ring',{rotation: 0}, {duration: 15, ease: "none", rotation: -90})
+    //          .fromTo('.ring',{rotation: -90}, {duration: 15, ease: "none", rotation: 0})
+    //          .to('.zone_cl', {duration: 1, ease: "none", scale: 1, rotation: 0})
+
+    // var zone_ring_gsap = gsap.timeline();
+    // zone_ring_gsap.fromTo('.zone_ring',{rotation: 0}, {duration: 60, ease: "none", rotation: -480})
+    //               .fromTo('.zone_ring',{rotation: -480}, {duration: 30, ease: "none", rotation: -240})
+
+    phaseOne = setInterval(function(){
+      if (count_animation <= 360){
+        if (reloadTime == 0){                                                                       //1
+            sound.stop();
+            reloadSound.play();
+        } else if (reloadTime == 2) {
+            sound.play();
+        };
+        reloadTime += 1;
+        if (count_animation > 0 && count_animation <= 120) {
+          jQuery('.zone_v2, .zone_d2_, .zone_d3, .zone_d4').css({background: '#fff url(/wp-content/themes/bcwish/img/disfunction.png) center center/100% no-repeat'});
+        } else if (count_animation > 120 && count_animation <= 220) {
+          jQuery('.zone_v2, .zone_d2_, .zone_d3, .zone_d4').css({background: '#fff url(/wp-content/themes/bcwish/img/travma.png) center center/100% no-repeat'});
+        } else if (count_animation > 220 && count_animation <= 240) {
+          jQuery('.zone_v2, .zone_d2_, .zone_d3, .zone_d4').css({background: '#fff url(/wp-content/themes/bcwish/img/povregdenie_demona.png) center center/100% no-repeat'});
+        } else if (count_animation > 240) {
+          jQuery('.zone_ring').css({background: '#fff url(/wp-content/themes/bcwish/img/daemon.png) center center/100% no-repeat'});
+        }
+        count_animation += 1;
+      } else {
+        clearInterval(phaseOne);
+        count_animation = 1;
+        jQuery('.zone_v2, .zone_d2_, .zone_d3, .zone_d4, .zone_cl').css({
+            background: '#fff',
+            color: '#413e66',
+            borderColor: '#413e66',
+            transform: 'rotate(0deg) scale(1)',
+            paddingTop: '2px',
+            zIndex: '2'
+        });
+        jQuery('.ring').css('transform', 'rotate(0deg)');
+        jQuery('.zone_ring').css('transform', 'rotate(0deg)');
+        sound.stop();
+        if (pausedStatus == true) {
+          localStorage.setItem('paused', 'universal_2_1');
+          endNow()
+        } else {
+          // universal_2_1();
+          console.log('continue');
+        } 
+      }
+    }, 250);
+  }
+
+// Если есть незавершенный протокол
+  if (localStorage.getItem('paused')) {
+    jQuery('.wizard_continue').removeClass('hidden');
+    returned_img = localStorage.getItem('pausedPhoto');
+    pausedStatus = true;
+    jQuery('.main_arrow').addClass('main_arrow_combine');
+    jQuery('.main_arrow_title').addClass('main_arrow_title_combine');
+  }
+
+  jQuery('.wizard_continue.btn-warning').on('click', function(event) {
+    jQuery('.machine_screen, #intro').addClass('hidden');
+    jQuery('.wizard_returned').attr('src', returned_img);
+    jQuery('.wm_start').removeClass('unopacity');
+    jQuery('.wm_start').removeAttr('style');
+    jQuery('.wizard_to_protList, .wizard_play, .wizard_starter_alt').fadeIn(500).removeClass('hidden');
+    jQuery('.wizard_main_screen').fadeIn(500).removeClass('hidden').css('display', 'flex');
+    jQuery('.wizard_heading').text('Перенесите зоны на фото и можно будет продолжить работу.');
+  });
+
+  
+  checkPoints = function(){
+    jQuery('.zone_movable').each(function() {
+      if(parseFloat(jQuery(this).css('left')) < 480){
+        pointsStatus = false;
+        // console.log('status '+' '+jQuery(this).text()+' '+jQuery(this).css('top')+' '+pointsStatus);
+      }
+      if (parseFloat(jQuery('.ring').css('left')) < 380) {
+        pointsStatus = false;
+      }
+    });
+  }
+
+  jQuery('.wizard_play, .wizard_starter_alt').on('click', function(event) {
+    checkPoints();
+    // if(pointsStatus == false){
+    //   swal("Не все зоны перенесены!", "Перед началом процедуры необходимо перенести на фото калибровочное кольцо и все зоны.", "info");
+    //   alert_altSound.play();
+    //   pointsStatus = true;
+    // } else {
+      if (pausedStatus == true) {
+        // jQuery('.wizard_returned').attr('src', localStorage.getItem('pausedPhoto'));
+        // console.log(localStorage.getItem('pausedPhoto'));
+        protocolfromMemory = eval(localStorage.getItem('paused'));
+        protocolfromMemory();
+      } else {
+        jQuery('.wizard_stop').removeClass('wizard_stop_inProgress');
+        var protocol = localStorage.getItem('cur_protocol');
+        if (protocol == 'v1') {
+          v1();
+          jQuery('.status_title').text('Протокол V1');
+        } else if (protocol == 'v2') {
+          v2();
+          jQuery('.status_title').text('Протокол V2-5');
+        } else if (protocol == 'v3') {
+          v3();
+          jQuery('.status_title').text('Протокол V3-4');
+        } else if (protocol == 'v4') {
+          v4();
+          jQuery('.status_title').text('Протокол V4-3');
+        } else if (protocol == 'v5') {
+          v5();
+          jQuery('.status_title').text('Протокол V5-2');
+        } else if (protocol == 'solis') {
+          solis();
+          jQuery('.status_title').text('Протокол Solis');
+        } else if (protocol == 'drenag') {
+          drenag();
+          jQuery('.status_title').text('Дренажный протокол');
+        } else if (protocol == 'universal') {
+          universal();
+          jQuery('.status_title').text('Универсальный протокол');
+        } else if (protocol == 'visceral') {
+          mmt();
+          jQuery('.status_title').text('Висцеральный протокол');
+        }
+      }
+      pausedStatus = false;
+      console.log('ding');
+      jQuery('.wizard_play, .wizard_starter_alt').addClass('hidden');
+      jQuery('.wizard_stop, .zone_ring').fadeIn(500).removeClass('hidden');
+      jQuery('.wizard_to_protList').addClass('prot_in_progress');
+      jQuery('.ring').addClass('in_progress');
+      localStorage.removeItem('paused');
+      localStorage.removeItem('pausedPhoto');
+      jQuery('.wizard_stop').removeClass('wizard_stop_inProgress');
+    // }
+  });
+
+
+  // STOP
+  function hideNote() {
+    jQuery('.wizard_stop').popover('hide');
+  }
+
+  jQuery('.wizard_stop') .on('click', function(event) {
+    jQuery(this).addClass('wizard_stop_inProgress');
+    jQuery('.header-title').text('Программа останавливается');
+    // endStatus = true;
+    jQuery('.wizard_stop').popover('show');
+    setTimeout(hideNote, 5000);
+    localStorage.setItem('pausedPhoto', jQuery('.wizard_returned').attr('src'));
+    pausedStatus = true;
+    // console.log('pausedStatus = true');
+  });
+
+});
