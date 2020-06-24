69f733b4c605c4aebfc17124d4079b7826a56ead
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-06-24 16:45:06 +0300

    estate-33

--- wp-content/themes/bcwish/index.php
+++ wp-content/themes/bcwish/index.php
@@ -443,7 +443,7 @@
                           <div class="zone_estate zone_wc zone_default">С</div>
                         </div>
                       </div>
-                      <div class="col-sm-10 returned_estate wow bounceInUp" data-wow-duration="1.4s" data-wow-delay="1.0s">
+                      <div class="col-sm-10 returned_estate wow bounceInUp" data-wow-duration="1.4s" data-wow-delay="0.4s">
                         <img src="" class="wizard_returned_estate draggable" alt="wizard_returned">
                       </div>
                     </div>
