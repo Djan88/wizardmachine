ae7a307e9f641067f1b099136c50ac3927a5395e
Ivan Avraamov <ivan.avraamov@gmail.com>
2020-09-19 09:42:52 +0300

    icons

--- wp-content/themes/bcwish/index.php
+++ wp-content/themes/bcwish/index.php
@@ -2,6 +2,7 @@
 <html class="all" lang="en">
 <head>
   <!-- Yandex.Metrika counter -->
+  <script src="https://kit.fontawesome.com/d5792f1ef9.js" crossorigin="anonymous"></script>
   <script type="text/javascript" >
      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
      m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
